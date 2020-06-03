140c1489e93d98536d8de3a965b9517bef7dc466
Michael Li <dev.mike011235@gmail.com>
2020-06-02 22:47:44 -0400

    added js

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,13 @@
-//selectors
+// Selectors
 var answerFeedbackEl = document.querySelector("#answer-feedback");
 var timer = document.querySelector("#timer");
 var mainContentBox = document.querySelector("#main-content");
 var scoreBoardButtonTopRight = document.querySelector("#top-right-sb-button");
 
+// Global variables
 var secondsLeft = 60;
 var totalScore = 0;
-var scoresBoardList = {};
+var scoresBoardList = {"name1": 1, "name2": 2, "name3": 3};
 
 //question and answers bank
 //how do you traverse this? 
@@ -46,83 +47,83 @@ function main () {
 // Fills content box with introduction
 // Has an event listener: 
 //      Calls generateQuesAnsContent();
-// function generateIntroContent () {
-//     var introStatement = document.createElement("h1");
-//     var introRules = document.createElement("p");
-//     var introButton = document.createElement("button");
+function generateIntroContent () {
+    var introStatement = document.createElement("h1");
+    var introRules = document.createElement("p");
+    var introButton = document.createElement("button");
     
 
-//     mainContentBox.appendChild(introStatement);
-//     introStatement.textContent = "Take the quiz to see how good you are at JS!";
-//     introStatement.setAttribute("style", "text-align: center");
+    mainContentBox.appendChild(introStatement);
+    introStatement.textContent = "Take the quiz to see how good you are at JS!";
+    introStatement.setAttribute("style", "text-align: center");
 
-//     mainContentBox.appendChild(introRules);
-//     introRules.textContent = "You have 60 seconds to answer 10 questions. Each correct answer awards you with 1 point. Each wrong answer brutally punishes you by subtracting 5 seconds off the clock.";
-//     introRules.setAttribute("style", "text-align: center");
+    mainContentBox.appendChild(introRules);
+    introRules.textContent = "You have 60 seconds to answer 10 questions. Each correct answer awards you with 1 point. Each wrong answer brutally punishes you by subtracting 5 seconds off the clock.";
+    introRules.setAttribute("style", "text-align: center");
 
-//     mainContentBox.appendChild(introButton);
-//     introButton.textContent = "START";
+    mainContentBox.appendChild(introButton);
+    introButton.textContent = "START";
 
-//     introButton.addEventListener("click", function() {
-//     event.preventDefault()
-//         clearContent();
-//     });
-// }
+    introButton.addEventListener("click", function() {
+    event.preventDefault()
+        clearContent();
+    });
+}
 
 // // Clears all the content in the main content box
-// clearContent() {
-//     mainContentBox.querySelectorAll('*').forEach(n => n.remove());
-// }
+function clearContent() {
+    mainContentBox.querySelectorAll('*').forEach(n => n.remove());
+}
 
-// function generateQuesAnsContent () {
-//     var question = document.createElement("h3");
-//     var answer = document.createElement("button")
-//     var correctAns;
+function generateQuesAnsContent () {
+    var question = document.createElement("h3");
+    var answer = document.createElement("button")
+    var correctAns;
 
-//     for (var i in questionBank) {
-//         question.textContent = i.question;
-//         console.log(question);
-//     }
+    for (var i in questionBank) {
+        question.textContent = i.question;
+        console.log(question);
+    }
 
-// }
+}
 
 
 //offer to enter name for the high score board
 //why does this give a token/syntax error??
-// function offerHighScore(totalScore) {
-//     var finishedNotification = document.createElement("h5");
-//     var displayTotalScore = document.createElement("p");
-//     var nameInputForm = document.createElement("form");
-//     var nameInputLabel = document.createElement("label");
-//     var nameInputField = document.createElement("input");
-//     var submitNameButton = document.createElement("button");
+function offerHighScore(totalScore) {
+    var finishedNotification = document.createElement("h5");
+    var displayTotalScore = document.createElement("p");
+    var nameInputForm = document.createElement("form");
+    var nameInputLabel = document.createElement("label");
+    var nameInputField = document.createElement("input");
+    var submitNameButton = document.createElement("button");
 
-//     // Create the content
-//     finishedNotification.textContent = "All Done!";
-//     displayTotalScore.textContent = "Your total score is " + totalScore + "."; 
-//     mainContentBox.appendChild(finishedNotification);
-//     mainContentBox.appendChild(displayTotalScore);
-//     mainContentBox.appendChild(nameInputForm);
-//     nameInputForm.appendChild(nameInputLabel);
-//     nameInputLabel.textContent = "Enter your name:";
-//     nameInputForm.appendChild(nameInputField);
-//     mainContentBox.appendChild(submitNameButton);
-//     submitNameButton.textContent = "submit score";
+    // Create the content
+    finishedNotification.textContent = "All Done!";
+    displayTotalScore.textContent = "Your total score is " + totalScore + "."; 
+    mainContentBox.appendChild(finishedNotification);
+    mainContentBox.appendChild(displayTotalScore);
+    mainContentBox.appendChild(nameInputForm);
+    nameInputForm.appendChild(nameInputLabel);
+    nameInputLabel.textContent = "Enter your name:";
+    nameInputForm.appendChild(nameInputField);
+    mainContentBox.appendChild(submitNameButton);
+    submitNameButton.textContent = "submit score";
 
-//     // Handle the input
-//     nameInputField.setAttribute("class", "nameInput");
-//     var playerName = nameInputField.getElementsByClassName("nameInput").value;
+    // Handle the input
+    nameInputField.setAttribute("class", "nameInput");
+    var playerName = nameInputField.getElementsByClassName("nameInput").value;
 
-//     submitNameButton.addEventListener("click", function(){
-//         event.preventDefault();
-//         topTenScores[playerName] = totalScore;
-//         clearContent();
-//     });
-// } 
+    submitNameButton.addEventListener("click", function(){
+        event.preventDefault();
+        topTenScores[playerName] = totalScore;
+        clearContent();
+    });
+} 
 
 
-//final score board
-//user does not interact with it
+// Final score board.
+// User does not interact with it.
 function scoreBoard () {
     var scoreBoardTitle = document.createElement("h3");
     var listOfScores = document.createElement("ol");
@@ -150,8 +151,8 @@ function scoreBoard () {
 }
 
 
-//timer count down
-//basic functionality. working. 
+// Timer count down
+// Basic functionality. Functional. 
 function startTimer() {
   var timerInterval = setInterval(function() {
     secondsLeft--;
@@ -169,9 +170,18 @@ function decreaseTimer(){
     secondsLeft -= 5;
 }
 
-scoreBoardButtonTopRight.addEventListener("click", function(){
-    scoreBoard();
-});
+// Called by Q&A function
+function feedback (bool) {
+    if (bool) {
+        setTimeout(function(){ answerFeedbackEl.textContent = "Correct!"; }, 2000);
+    } else {
+        setTimeout(function(){ answerFeedbackEl.textContent = "Wrong!"; }, 2000);
+        decreaseTimer();
+    }
+}
+
+//global event listeners
+scoreBoardButtonTopRight.addEventListener("click", scoreBoard());
 
 
 /* Strategy ++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -247,7 +257,7 @@ generateQuesAnsContent(){
     decreases time by 5 seconds if wrong answer is given. 
 }
 
-feedbackText(bool){
+@feedbackText(bool){
     called when a answer is selected.
     if correct, display correct
     if not, display wrong.
@@ -266,6 +276,8 @@ call back function{
     call scoreBoard()
 
 }
+
 @ = created
 @? = created, but has bugs
+@* = implemented, no syntax errors, have not run functional test yet
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
